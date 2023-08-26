0dcff711f94efd250c61bc6b51187c26031e6c42
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-08-26 11:07:33 +0500

    comit

---
+++ main.js
@@ -0,0 +1,23 @@
+
+let soz1 = ["sna","gap","namgap","rigth" ]
+console.log(soz1[3]);
+
+// let soz = "salom dunyo" 
+// console.log(soz.split().reverse.join(""));
+
+
+let soz = "1java2cscript3 5deve6lo4per"
+function str(num,str2) {
+    let obj ={ num,str2}
+    for(let i; i<soz.length; i++){
+        if (typeof i == "number") {
+            num += i
+        } else {
+            str2 +=i
+        }
+        //  obj ={num,str2}
+    }
+return num
+    
+}
+console.log(str());
