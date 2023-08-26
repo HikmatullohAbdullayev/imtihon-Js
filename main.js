1ff40fa81bd0ac0fa112c44d59f305484ddad7f6
HikmatullohAbdullayev <hikmatullohabdullayev2@gmail.com>
2023-08-26 11:14:23 +0500

    commit

--- main.js
+++ main.js
@@ -1,23 +1,25 @@
 
-let soz1 = ["sna","gap","namgap","rigth" ]
-console.log(soz1[3]);
+// let soz1 = ["sna","gap","namgap","rigth" ]
+// console.log(soz1[3]);
 
 // let soz = "salom dunyo" 
 // console.log(soz.split().reverse.join(""));
 
 
-let soz = "1java2cscript3 5deve6lo4per"
-function str(num,str2) {
-    let obj ={ num,str2}
-    for(let i; i<soz.length; i++){
-        if (typeof i == "number") {
-            num += i
-        } else {
-            str2 +=i
-        }
-        //  obj ={num,str2}
-    }
-return num
+// let soz = "1java2cscript3 5deve6lo4per"
+// function str() {
+//     let num = undefined
+//     let str2 = undefined
+//     let obj ={ num,str2}
+//     for(let i; i<soz.length; i++){
+//         if (typeof i == "number") {
+//             num += i
+//         } else {
+//             str2 +=i
+//         }
+//          obj ={num,str2}
+//     }
+// return num
     
-}
-console.log(str());
+// }
+// console.log(str());
